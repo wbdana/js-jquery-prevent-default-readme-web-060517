651919268341a940226c2601ae54794b7d44dee8
wbdana <william.b.dana@gmail.com>
2017-07-21 13:46:42 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-  
+
   // browser refreshes on submit
-  $('form').on('submit', function(){
+  // $('form').on('submit', function(){
+  //   var name = $('#name').val();
+  //   $("#hello").text("Hello, " + name);
+  // });
+
+  // examine event object
+  $('form').on('submit', function(event){
     var name = $('#name').val();
     $("#hello").text("Hello, " + name);
+    debugger;
+    event.preventDefault();
   });
 
-  //examine event object
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   debugger;
-  //   event.preventDefault();
-  // });
-
   // stop page refresh
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   event.preventDefault();
-  // });
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    event.preventDefault();
+  });
 
 });
